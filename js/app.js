bfa875f2553b267c56d6febf27d9d9e97fec76b3
Sardor <sindarovasardor9@gmail.com>
2022-12-22 12:43:59 +0500

    55

---
+++ js/app.js
@@ -0,0 +1,20 @@
+// (async function getAlbums() {
+//   let apie = await fetch("https://jsonplaceholder.typicode.com/albums");
+//   let api = await apie.json();
+  
+//   console.log(api);
+// })();
+// (async function getAlbums() {
+//   let apie = await fetch("https://jsonplaceholder.typicode.com/photos");
+//   let api = await apie.json();
+  
+//   console.log(api);
+// })();
+(async function getAlbums() {
+  let apie = await fetch("https://jsonplaceholder.typicode.com/comments",
+  {
+    method:"POST",
+    body: JSON.stringify({title:"test", userId:3})
+  });
+  
+})();
